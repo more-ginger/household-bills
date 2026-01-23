da45a4ba78a07cb5ca2cc4440d19ea9b5cb96df0
fmorini <33264532+more-ginger@users.noreply.github.com>
2026-01-23 13:37:17 +0100

    feat: add first batch of assets

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,7 +1,21 @@
 <section class="min-h-dvh">
 	<h1 class="text-6xl">Falling Behind on Household Bills</h1>
 	<h2 class="text-3xl">Risk Factors and Population Trends in the UK since 2009</h2>
-	<p>
-		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
-	</p>
+	<div class="w-60">
+		<img src="illustrations/webp/bill.webp" />
+	</div>
+	<div class="w-40">
+		<img src="illustrations/webp/bill.webp" />
+	</div>
+	<div class="w-20">
+		<img src="illustrations/webp/bill.webp" />
+	</div>
+	<div class="flex">
+		<div class="m-2 w-20 border">
+			<img class="" src="illustrations/webp/human1-darkblue-accent.webp" />
+		</div>
+		<div class="m-2 w-20 border">
+			<img class="" src="illustrations/webp/human2-darkblue-accent.webp" />
+		</div>
+	</div>
 </section>
