96193634bb9286ba33eb71eb27c385f6987b96bc
fmorini <33264532+more-ginger@users.noreply.github.com>
2026-02-04 16:53:06 +0100

    fix: replace variable height illustrations with fixed height

--- src/lib/components/scrollytelling/ScrollyFigure.svelte
+++ src/lib/components/scrollytelling/ScrollyFigure.svelte
@@ -2,13 +2,13 @@
 	import FigureData from '$lib/data/figure.json';
 
 	let { step } = $props();
-	$inspect('from figure', step);
+	$inspect('from figure', FigureData['figure-steps'][step]);
 </script>
 
-{#if step === 0}
-	<div class="align-center m-auto flex h-full w-4/7 flex-wrap content-center justify-items-center">
-		{#each FigureData['figure-steps'][2].urls as figure}
-			<div><img class="mx-2 h-20" src={figure} /></div>
-		{/each}
-	</div>
-{/if}
+<div
+	class="align-center m-auto flex h-full w-6/7 flex-wrap content-center justify-center justify-items-stretch"
+>
+	{#each FigureData['figure-steps'][step].urls as figure}
+		<div><img class="max-h-100" src={figure} /></div>
+	{/each}
+</div>
--- src/lib/content/test.md
+++ src/lib/content/test.md
@@ -1,4 +1,7 @@
 <div class="has-step">
     <div class="step"><div class="step-child">What if I have a div with a special class?</div></div>
     <div class="step"><div class="step-child">And then another one?</div></div>
+    <div class="step"><div class="step-child">And then another one?</div></div>
+    <div class="step"><div class="step-child">And then another one?</div></div>
+    <div class="step"><div class="step-child">And then another one?</div></div>
 </div>
