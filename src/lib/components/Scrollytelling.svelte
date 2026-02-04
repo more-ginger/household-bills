9bd2f6733dd5eb1e953dfa0a6ab78eadaa6ad3f4
fmorini <33264532+more-ginger@users.noreply.github.com>
2026-02-04 15:23:59 +0100

    feat: add logic to load scroll

--- src/lib/components/Scrollytelling.svelte
+++ src/lib/components/Scrollytelling.svelte
@@ -1,11 +1,42 @@
 <script lang="ts">
-	import { marked } from 'marked';
+	import scrollama from 'scrollama';
 	import Text from '../content/test.md?raw';
+	import ScrollyBlocks from './scrollytelling/ScrollyBlocks.svelte';
 
-	const html = marked.parse(Text);
+	const scroller = scrollama();
+	let step: number = $state(0);
+	let direction: 'up' | 'down' | null = $state(null);
+
+	let textIsRendered: boolean = $state(false);
+	let textHasSteps: boolean = $state(false);
+
+	// the event is dispatched from BlockRenderer component if scrolly
+	function onTextRender(data: { isTextRendered: boolean; hasSteps: boolean }) {
+		textIsRendered = data.isTextRendered;
+		textHasSteps = data.hasSteps;
+	}
+
+	$effect(() => {
+		setTimeout(() => {
+			scroller
+				.setup({
+					step: '.step',
+					debug: false,
+					offset: 0.7
+				})
+				.onStepEnter((response) => {
+					step = response.index;
+					direction = response.direction;
+
+					console.log(step);
+				})
+				.onStepProgress((response) => {})
+				.onStepExit((response) => {});
+		}, 200);
+	});
 </script>
 
 <div class="relative">
 	<figure class="sticky top-0 h-dvh w-full bg-red-100"></figure>
-	<article class="relative">{@html html}</article>
+	<article class="relative"><ScrollyBlocks rawScrollyText={Text} {onTextRender} /></article>
 </div>
--- src/lib/content/test.md
+++ src/lib/content/test.md
@@ -1,2 +1,4 @@
-<div class="step"><div class="step-child">What if I have a div with a special class?</div></div>
-<div class="step"><div class="step-child">And then another one?</div></div>
+<div class="has-step">
+    <div class="step"><div class="step-child">What if I have a div with a special class?</div></div>
+    <div class="step"><div class="step-child">And then another one?</div></div>
+</div>
