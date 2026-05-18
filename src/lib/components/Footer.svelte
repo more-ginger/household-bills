586011bf35907becdce123ac454fef41c5f82d1d
fmorini <33264532+more-ginger@users.noreply.github.com>
2026-05-18 14:12:28 +0200

    feat: footer with logos

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,1 +1,20 @@
-<h1>Footer</h1>
+<div class="grid h-32 grid-cols-1 items-center gap-6 bg-tertiary-blue pl-8">
+	<div class="grid grid-cols-2 items-stretch justify-stretch gap-6">
+		<div class="p-4">
+			<div class="grid grid-cols-5 items-center gap-6">
+				<img src="logos/uol.png" />
+				<img src="logos/esrc.png" />
+				<img class="w-[60%] place-self-center" src="logos/citizens_advice.png" />
+				<img class="" src="logos/uclab.png" />
+				<img src="logos/fhp.png" />
+			</div>
+		</div>
+		<div class="grid grid-cols-5 place-items-end items-center gap-6 p-6 font-epilogue">
+			<div></div>
+			<div></div>
+			<div></div>
+			<a class="" href="https://www.fh-potsdam.de/impressum">Imprint</a>
+			<a class="" href="https://www.fh-potsdam.de/en/privacy">Privacy Policy</a>
+		</div>
+	</div>
+</div>
