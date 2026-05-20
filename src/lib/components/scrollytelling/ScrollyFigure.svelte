07fe61e6f75aa6c6ef31f16383fe18c7ff260477
fmorini <33264532+more-ginger@users.noreply.github.com>
2026-05-20 20:01:28 +0200

    feat: references

--- src/lib/components/scrollytelling/ScrollyFigure.svelte
+++ src/lib/components/scrollytelling/ScrollyFigure.svelte
@@ -4,6 +4,7 @@
 	import { fade } from 'svelte/transition';
 
 	let { step } = $props();
+	$inspect(FigureData['figure-steps'][step]['has-footnote']);
 </script>
 
 <div class="relative h-full w-full">
@@ -11,11 +12,27 @@
 		<div
 			in:fade={{ duration: 400, delay: 150 }}
 			out:fade={{ duration: 250 }}
-			class={`absolute inset-0 align-center m-auto content-center justify-center ${FigureData['figure-steps'][step].title === 'population' ? 'grid w-[70vw] grid-cols-10 grid-rows-6 py-10' : 'flex w-6/7 flex-wrap'}`}
+			class={`align-center absolute inset-0 m-auto content-center justify-center ${FigureData['figure-steps'][step].title === 'population' ? 'grid w-[70vw] grid-cols-10 grid-rows-6 py-10' : 'flex w-6/7 flex-wrap'}`}
 		>
 			{#each FigureData['figure-steps'][step].urls as figure, f}
-				<div><img class={`${FigureData['figure-steps'][step].css}`} src={resolve(figure)} alt="" /></div>
+				<div>
+					<img class={`${FigureData['figure-steps'][step].css}`} src={resolve(figure)} alt="" />
+				</div>
 			{/each}
+			{#if FigureData['figure-steps'][step]['has-footnote']}
+				<div class="absolute right-0 bottom-10 flex w-2/6 items-center justify-end text-xs italic">
+					<img
+						class={`${FigureData['figure-steps'][step].title === 'population' ? 'order-1 h-[4em] pr-4' : 'order-2 h-[1.5em] pl-4'} w-auto`}
+						src={resolve(FigureData['figure-steps'][step].footnote.img)}
+						alt=""
+					/>
+					<div
+						class={`${FigureData['figure-steps'][step].title === 'population' ? 'order-2 text-left' : 'order-1 text-right'}`}
+					>
+						{FigureData['figure-steps'][step].footnote.text}
+					</div>
+				</div>
+			{/if}
 		</div>
 	{/key}
 </div>
